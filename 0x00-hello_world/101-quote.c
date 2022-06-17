9de030ef7b553084048437df1e6789304eba360a
Precious Edmund <edmundprecious23@gmail.com>
2022-06-17 08:23:26 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * main - Entry Point
+ * main - Entry point
  *
  * Return: Always 1 (Error)
  */
@@ -10,3 +10,4 @@ int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
+}
